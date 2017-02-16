50acbf308efd8eea149a6891f91d96b0f23a5030
PierreHachard <p.hachard.13@groupe-esigelec.fr>
2017-02-16 18:13:08 +0100

    Récupération jeu et accueil

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Accueil.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Accueil : Form
+    {
+        public Accueil()
+        {
+            InitializeComponent();
+        }
+
+        private void Accueil_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnexion_Click(object sender, EventArgs e)
+        {
+            //String nomDeCompte = nomCompte.Text;
+            Console.WriteLine("Nom de compte : "+nomCompte.Text+", mot de passe : "+password.Text);
+
+            //On vérifie que l'utilisateur/mot de passe appartient à la base de données
+
+            //si il a pu se connecter
+            //Application.Exit();
+            Jeu formJeu = new Jeu(this);
+            formJeu.Show();
+            //this.Hide();// = false;
+            //this.Visible = false;
+        }
+
+        private void btnInscription_Click(object sender, EventArgs e)
+        {
+            //On inscrit l'utilisateur dans la base de données (si son nom de compte n'existe pas déjà)
+            MessageBox.Show(this, "Inscription réussie", "Casse-Brique", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Jeu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Jeu : Form
+    {
+        private Accueil accueil;
+        public Jeu(Accueil accueil)
+        {
+            InitializeComponent();
+            this.accueil = accueil;
+            this.accueil.Visible = false;
+        }
+
+        private void Jeu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Jeu_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.accueil.Visible = true;
+        }
+
+    }
+}
