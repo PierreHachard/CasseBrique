2ac2c0b55cc9a67dcae275182ca1280738997cfd
MaximeAndre <maxime.andre28@hotmail.fr>
2017-02-16 14:36:20 +0100

    Push 1

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -10,11 +10,12 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class InscForm : Form
     {
-        public Form1()
+        public InscForm()
         {
             InitializeComponent();
         }
+
     }
 }
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/RectangleGraphique.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Windows.Shapes;
+
+public class RectangleGraphique : Rectangle
+{
+	public RectangleGrapgique()
+	{
+	}
+}
